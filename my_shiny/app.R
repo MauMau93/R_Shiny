a2ae30777684e678953958449cc972c08af4c2a7
MauMau93 <mauriciomarcosf@gmail.com>
2021-03-03 03:05:56 +0100

    first commit

--- my_shiny/app.R
+++ my_shiny/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
@@ -15,6 +6,9 @@ data(ArcheryData)
 
 ArcheryData$Sex = factor(ifelse(ArcheryData$Sex == "m", "Male" , "Female"))
 list_choices <-  unique(ArcheryData$Sex)
+list_choices2 <- colnames(ArcheryData[-3][-6])
+list_choices3 <- colnames(ArcheryData[-3][-6])
+
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("Archery Data App",
@@ -25,6 +19,12 @@ ui <- navbarPage("Archery Data App",
                               sidebarLayout(
                                   sidebarPanel(
                                       selectInput("select", label = h3("Plot participants according to Sex"), 
+                                                  choices = character(0),
+                                                  selected = 1),
+                                      selectInput("select2", label = h3("Choose Variable for x-axis"), 
+                                                  choices = character(0),
+                                                  selected = 1),
+                                      selectInput("select3", label = h3("Choose Variable for y-axis"), 
                                                   choices = character(0),
                                                   selected = 1)
                                   ),
@@ -45,9 +45,18 @@ server <- function(input, output, session) {
                       choices = list_choices,
                       selected = tail(list_choices, 1)
     )
+    updateSelectInput(session, "select2",
+                      choices = list_choices2,
+                      selected = tail(list_choices2, 1)
+    )
+    updateSelectInput(session, "select3",
+                      choices = list_choices3,
+                      selected = tail(list_choices3, 1)
+    )
     output$plot <- renderPlot({
-        ggplot(ArcheryData  %>% filter(Sex == input$select), aes(Attendance, Improvement, colour = Sex)) +
-            scale_x_log10() +
+        ejex <- as.numeric(input$select2)
+        ejey <- as.numeric(input$select3)
+        ggplot(ArcheryData  %>% filter(Sex == input$select), aes(x = ejex ,y= ejey, colour = Sex)) +
             col_scale +
             geom_point()
     })
